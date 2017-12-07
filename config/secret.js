0f9e9325e36ae43c58641779d6aa657182d0bd40
Arifin <arifin.1@hotmail.com>
2017-12-07 08:23:18 +0700

    after mongoosastic

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://root:arifin@ds119486.mlab.com:19486/db_latihan',
+  port: 3000,
+  secretKey: 'project123'
+};
--- server.js
+++ server.js
@@ -1,21 +1,69 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejsMate = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
+
+var secret = require('./config/secret');
+var Category = require('./models/category');
 
 var app = express();
 
+mongoose.connect(secret.database, function(err) {
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Connected to the database");
+  }
+});
+
 // Middleware
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
-
-app.get("/", function(req, res) {
-  var name = "Arifin";
-  res.json("My name is "+name);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secret.secretKey,
+  store: new MongoStore({ url: secret.database, autoReconnect: true })
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
 });
 
-app.get("/catname", function(req, res) {
-  res.json("batman");
+app.use(function(req, res, next) {
+  Category.find({}, function(err, categories) {
+    if (err) return next(err);
+    res.locals.categories = categories;
+    next();
+  });
 });
 
-app.listen(3000, function(err) {
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+var adminRoutes = require('./routes/admin');
+var apiRoutes = require('./api/api');
+app.use(mainRoutes);
+app.use(userRoutes);
+app.use(adminRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(secret.port, function(err) {
   if(err) throw err;
   console.log("Server is running");
 });
